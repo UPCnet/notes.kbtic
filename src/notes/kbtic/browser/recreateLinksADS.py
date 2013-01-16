ed82a467ef1a3ca0de1869522985ba7ae873aec0
Roberto Diaz <roberto.diaz@upcnet.es>
2013-01-16 09:23:37 +0100

    recreateLinks

--- src/notes/kbtic/browser/recreateLinksADS.py
+++ src/notes/kbtic/browser/recreateLinksADS.py
@@ -19,16 +19,21 @@ class recreateLinks():
             if "#Link" in line:
                 link_intern = line.split(' ')[9].upper()
                 if "DOCADS.NSF" in link_intern:  # Mirem si es link a mateixa BBDD
+                    # Agafem contingut html de la pàgina
                     HTML_PAGE_WITH_LINK = requests.get(str(line.split(' ')[7]), auth=('admin', 'admin')).content
                     obj = self.context.portal_catalog.searchResults(portal_type='notesDocument', path='/kbtic/ads-spo', id=line.split(' ')[7].replace('\n', '').split('/')[-1:])[0]
-                    newHTML = re.search(r'parent-fieldname-body">(.*?)viewlet-below-content-body">', HTML_PAGE_WITH_LINK, re.DOTALL | re.MULTILINE).groups()[0][:-100]
+                    try:
+                        newHTML = re.search(r'parent-fieldname-body">(.*?)viewlet-below-content-body">', HTML_PAGE_WITH_LINK, re.DOTALL | re.MULTILINE).groups()[0][:-100]
+                    except:
+                        newHTML = ""
+                        logging.info("----------- ERROR: Content corrupte. Es deixa en blanc...: %s ", obj.absolute_url())
                     NotesUID2Search = link_intern.split('/')[-1:][0].replace('?OPENDOCUMENT', '').replace('\n', '')  # Esta en mays
                     titleLink, nouLink = self.searchNotesDoc(NotesUID2Search)
                     url2search = ('/' + '/'.join(line.split(' ')[9].split('/')[3:])).replace('\n', '')
                     nouLink = '/' + '/'.join(nouLink.split('/')[4:])
                     replacedContent = newHTML.replace(url2search, nouLink)
                     replacedContent = replacedContent.replace('\r\n', '')
-                    replacedContent = replacedContent.replace(("Database 'ADS - SPO', View 'By Category', Document '"), '')
+                    replacedContent = replacedContent.replace(("Database 'Documentació ADS i SPO', View 'By Category', Document '"), '')
                     replacedContent = replacedContent.replace("/icons/ecblank.gif", '/++resource++notes_kbtic_images/ecblank.gif')
                     replacedContent = replacedContent.replace("/icons/doclink.gif", '/++resource++notes_kbtic_images/doclink.gif')
                     replacedContent = replacedContent.replace("/icons/collapse.gif", '/++resource++notes_kbtic_images/collapse.gif')
@@ -38,14 +43,14 @@ class recreateLinks():
                     objecte.setBody(replacedContent)
                     objecte.indexObject()
                     transaction.commit()
-                    changesFile.write('LinkChanged_in: ' + objecte.absolute_url() + '\n')
-                    logging.info("LinkChanged_in: %s ", objecte.absolute_url())
+                    changesFile.write('-Link_changed_in: ' + objecte.absolute_url() + '\n')
+                    logging.info("-Link_changed_in: %s ", objecte.absolute_url())
                     lista = lista + objecte.absolute_url() + '\n'
 
                 else:  # Sino, es un link extern no el fem...
                     original_plone_url = line.split(' ')[7]
-                    changesFile.write('ExternLink: ' + link_intern.replace('\n', '') + ' in: ' + original_plone_url + '\n')
-                    logging.info("ExternLink: %s in : %s", link_intern.replace('\n', ''), original_plone_url)
+                    changesFile.write('-ExternLink: ' + link_intern.replace('\n', '') + ' in: ' + original_plone_url + '\n')
+                    logging.info("-ExternLink: %s in : %s", link_intern.replace('\n', ''), original_plone_url)
         changesFile.write('-----------------------------------------------------------------------------' + '\n')
         changesFile.write('                             END                                             ' + '\n')
         changesFile.write('-----------------------------------------------------------------------------' + '\n')
