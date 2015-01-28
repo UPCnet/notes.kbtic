621592f83f4b38d9302298b1ab8fd8c03e3b4ed9
Paco Gregori <francisco.gregori@upcnet.es>
2015-01-28 16:28:54 +0100

    buscador desde documento

--- src/notes/kbtic/browser/scripts/search.js
+++ src/notes/kbtic/browser/scripts/search.js
@@ -127,6 +127,117 @@ $("a.CatItem").on("click", function (event) {
 
 });
 
+
+// ========================================================================== //
+
+    (function($) {  
+        $.getValue = function(key)   {  
+            key = key.replace(/[\[]/, '\\[');  
+            key = key.replace(/[\]]/, '\\]');  
+            var pattern = "[\\?&]" + key + "=([^&#]*)";  
+            var regex = new RegExp(pattern);  
+            var url = unescape(window.location.href);  
+            var results = regex.exec(url);
+            //alert(url);
+            if (results === null) {  
+                return null;  
+            } else {  
+                return results[1];  
+            }  
+        }  
+    })(jQuery);
+
+var getVal = $.getValue("cat");
+
+$("a.searchRedirect").on("click", function (event) {
+    event.preventDefault();
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+    var category = $(this).attr("value");
+    var host = $(location).attr('host');
+    $(location).attr('href','http://'+host+'/kbtic/kbtic-rin/?cat='+category);
+});
+
+
+
+if (getVal != null) {
+    var category = getVal
+
+    //$('#searchbytag').select2('destroy');
+    if ($("#searchbytag").val() == ''){
+        $('#searchbytag').select2({
+            tags: [],
+            tokenSeparators: [","],
+            minimumInputLength: 1,
+            ajax: {
+                url: portal_url + '/getVocabularies?name=plone.app.vocabularies.Keywords',
+                data: function (term, page) {
+                    return {
+                        query: term,
+                        page: page // page number
+                    };
+                },
+                results: function (data, page) {
+                    return data;
+                }
+            },
+            initSelection: function (element, callback) {
+                callback({id: category, text: category });
+            }
+        }).select2('val', []);
+        $("#searchbytag").val(category).trigger("change");
+    }
+
+    else{
+
+        var catSelect = $("#searchbytag").val().split(",");
+         $('#searchbytag').select2({
+            tags: [],
+            tokenSeparators: [","],
+            minimumInputLength: 1,
+            ajax: {
+                url: portal_url + '/getVocabularies?name=plone.app.vocabularies.Keywords',
+                data: function (term, page) {
+                    return {
+                        query: term,
+                        page: page // page number
+                    };
+                },
+                results: function (data, page) {
+                    return data;
+                }
+            },
+            initSelection: function (element, callback) {
+
+                var data = [];
+                
+                for(var i = 0; i < catSelect.length; i++){
+                    data.push({id:catSelect[i],text:catSelect[i]});
+                }
+                data.push({id: category, text: category});
+                callback(data);
+
+            }
+        }).select2('val', []);
+
+        var catToSearch = $("#searchbytag").val();
+        $("#searchbytag").val(catToSearch).trigger("change");
+    }
+
+}
+
+    // ========================================================================= //
+
+
+
+
+
+
+
+
+
+
+
 //fin
 });
 
